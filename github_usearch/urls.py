ac51d8c5da82cea252c9610a5017c8b890d7e1f9
Ankur Gupta <ankur0493@gmail.com>
2017-09-27 01:20:26 +0530

    Added user detail template+view

--- github_usearch/urls.py
+++ github_usearch/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 
 from .api import GithubUserSearchView
-from .views import AdminPanelHomeView, AdminPanelReportView
+from .views import UserListView, UserDetailView, ReportView
 
 
 github_api_urls = [
@@ -10,6 +10,8 @@ github_api_urls = [
 
 urlpatterns = [
     url(r'^github/', include( github_api_urls )),
-    url(r'admin_panel/reports/$', AdminPanelReportView.as_view()),
-    url(r'admin_panel/$', AdminPanelHomeView.as_view())
+    url(r'admin_panel/reports/$', ReportView.as_view()),
+    url(r'admin_panel/users/$', UserListView.as_view(), name="admin-user-list"),
+    url(r'admin_panel/users/(?P<username>.*)$', UserDetailView.as_view(), name="admin-user-detail"),
+    url(r'admin_panel/$', UserListView.as_view())
 ]
--- github_usearch/views.py
+++ github_usearch/views.py
@@ -2,25 +2,42 @@ from __future__ import unicode_literals
 
 import datetime
 
-from django.views.generic.base import TemplateView
+from django.views import generic
+from django.shortcuts import get_object_or_404
 
 from .models import GithubUserData
 
-class AdminPanelHomeView(TemplateView):
+class UserListView(generic.list.ListView):
 
-    template_name = "admin_panel.html"
+    model = GithubUserData
+    template_name = "user_list.html"
+    paginate_by = 15
 
     def get_context_data(self, **kwargs):
-        context = super(AdminPanelHomeView, self).get_context_data(**kwargs)
-        context['users'] = GithubUserData.objects.all().order_by('-created')[:10]
+        context = super(UserListView, self).get_context_data(**kwargs)
+        #context['users'] = GithubUserData.objects.all().order_by('-created')[:10]
         return context
 
-class AdminPanelReportView(TemplateView):
+
+class UserDetailView(generic.detail.DetailView):
+
+    model = GithubUserData
+    template_name = "user_detail.html"
+
+    def get_object(self, **kwargs):
+        return get_object_or_404(self.model, username=self.kwargs.get("username", ""))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        return context
+
+
+class ReportView(generic.base.TemplateView):
 
     template_name = "admin_panel_reports.html"
 
     def get_context_data(self, **kwargs):
-        context = super(AdminPanelReportView, self).get_context_data(**kwargs)
+        context = super(ReportView, self).get_context_data(**kwargs)
         today = datetime.datetime.today()
         context['users_today'] = GithubUserData.objects.filter(
             created__day=today.day, created__month=today.month, created__year=today.year).count()
